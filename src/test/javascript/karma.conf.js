ccf9c887992e1b4742b708a616b558e8a73de19d
Emily Klein <kleiner617@gmail.com>
2016-09-18 11:07:27 -0400

    Added lodash and angularjs-dropdown-multiselect to bower.json

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -26,7 +26,6 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/jquery/dist/jquery.js',
             'src/main/webapp/bower_components/json3/lib/json3.js',
             'src/main/webapp/bower_components/messageformat/messageformat.js',
-            'src/main/webapp/bower_components/lodash/dist/lodash.compat.js',
             'src/main/webapp/bower_components/d3/d3.js',
             'src/main/webapp/bower_components/angular/angular.js',
             'src/main/webapp/bower_components/angular-aria/angular-aria.js',
